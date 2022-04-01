083d507d89f76325f86c1f102862412bc405c9d7
Krzysztof Osuch <krzysiekos@gmail.pl>
2022-04-01 22:27:44 +0200

    lokalizacja ale nie śmiga

--- HomePage.js
+++ HomePage.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, Button, View, Alert } from 'react-native';
-
+import Travel from './location';
 export const HomePage = ({ navigation }) => {
   return (
-    <Button
-      title="Go to Tracker"
-      onPress={() =>
-        navigation.navigate('Tracker', { distance: 1000, price: 6.70 })
-      }
-    />
+    <View>
+      <Button
+        title="Go to Tracker"
+        onPress={() =>
+          navigation.navigate('Tracker', { distance: 1000, price: 6.70 })
+        }
+      />
+      <Button
+        title="Testuj dżipiesa"
+        onPress={() => {
+          try {
+            const t = new Travel();
+            t.startTravel();
+            setTimeout(() => {
+              console.log(t.traveledDistance)
+            }, 1000);
+          } catch (e) {
+            console.error(e);
+          }
+        }}
+      />
+    </View>
   );
 };
